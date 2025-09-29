84f67f0ad7a928ff1103b4cb0c8c0697b08a08d8
Anisha Pandey <pandeyanisha889@gmail.com>
2025-09-29 16:16:04 +0530

    Create NumPyramid.java

---
+++ NumPyramid.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NumPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of rows");
+        int r = sc.nextInt();
+
+        for (int i=1;i<=r;i++){
+            for(int j=1;j<=r-i;j++){
+                System.out.print(" ");
+            }
+            for (int l=1;l<=i;l++){
+                System.out.print(l);
+            }
+            for (int k=i-1;k>=1;k--){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
