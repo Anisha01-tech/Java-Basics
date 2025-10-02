7127632fc3370f6d0be258d3df2995d12c3a4166
Anisha Pandey <pandeyanisha889@gmail.com>
2025-10-02 20:12:50 +0530

    Create ReverseAnArray.java

---
+++ ReverseAnArray.java
@@ -0,0 +1,23 @@
+public class Reverse_An_Array {
+    static int[] reverseArray(int[] arr){
+        int n=arr.length;
+        int[] ans=new int[n];
+        int j=0;
+        //traverse original array in reverse direction
+        for(int i=n-1;i>=0;i--){
+            ans[j++]=arr[i]; //j++ is post increment operator first is print the value then increment it
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int a=9;
+        int b=3;
+        int[] arr={1,2,3,4,5};
+        int[] ans=reverseArray(arr);
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i] + " ");
+        }
+    }
+}
