ab242988f3b51e206ff88bb6175840daf0d99c67
Anisha Pandey <pandeyanisha889@gmail.com>
2025-10-04 18:47:04 +0530

    Create SumOfTheSeries.java

---
+++ SumOfTheSeries.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SumTheSeries {     //1-2+3-4......
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the no. of element in series");
+        int n=sc.nextInt();
+        int ans=0;
+        for (int i=1;i<=n;i++){
+            if(i%2==0){
+                ans-=i;
+            }else{
+                ans+=i;
+            }
+        }
+        System.out.println(ans);
+    }
+}
