ccc6dcf2856f7100965af464d180b102d233cbff
Anisha Pandey <pandeyanisha889@gmail.com>
2025-10-04 18:43:16 +0530

    Create SumOfDigitInNum.java

---
+++ SumOfDigitInNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumOfDigitInNum {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter number");
+        int n=sc.nextInt();
+        int sum=0;
+        int original_n=n;
+        while(n>0){
+            sum+=n%10;
+            n=n/10;
+
+        }
+        System.out.println(sum);
+    }
+}
