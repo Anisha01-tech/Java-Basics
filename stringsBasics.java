1219390dbd7613e0e6f6add9bd21ab79416efcda
Anisha Pandey <pandeyanisha889@gmail.com>
2025-11-11 11:24:43 +0530

    Create stringsBasics.java with string examples
    Add a basic Java program demonstrating string declaration and user input handling.

---
+++ stringsBasics.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class StringsBasics {
+    public static void main(String[] args) {
+       
+        // Declaration
+        String str="Anisha Pandey";
+        System.out.println(str);
+
+        //user input
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a string:");
+        //only print upto the space
+        String s=sc.next();
+
+        // print after space 
+        String sr=sc.nextLine();
+
+    }
+    
+}
