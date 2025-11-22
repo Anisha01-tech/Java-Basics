238bb0fde5a547683d0ab5029add4b60d7383f18
Anisha Pandey <pandeyanisha889@gmail.com>
2025-11-22 17:00:47 +0530

    Create ReverseEachWord.java

---
+++ ReverseEachWord.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class ReverseEachWord {
+    public static void main(String[] args) {
+        String str="I am an online student";
+        String ans="";
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch!=' '){
+                sb.append(ch);
+            }
+            else{
+                sb.reverse();
+                ans+=sb;
+                ans+=" ";
+                sb= new StringBuilder("");
+            }
+        }
+        sb.reverse();
+        ans+=sb;
+
+        System.out.println(ans);
+        
+    }
+    
+}
