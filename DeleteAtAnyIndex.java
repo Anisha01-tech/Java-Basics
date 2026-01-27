c462fec4e8f6a983f594e2c5ccd9c57f4583f09a
Anisha Pandey <pandeyanisha889@gmail.com>
2026-01-27 09:44:41 +0530

    Create DeleteAtAnyIndex.java

---
+++ DeleteAtAnyIndex.java
@@ -0,0 +1,103 @@
+public class DeleteAtIndex {
+    public static class Node{         
+        int data; 
+        Node next; 
+
+        Node(int data){
+            this.data=data;
+        }
+    }
+  
+    public static class linkedlist{
+        Node head=null;
+        Node tail=null;
+        void insertAtEnd(int val){
+            Node temp=new Node(val);
+            if(head==null){   // list empty
+                head=temp;
+                tail=temp;
+            }
+            else{
+                tail.next=temp;
+            }
+            tail=temp;
+        }
+        void insertAtStart(int val){
+            Node temp=new Node(val);
+            if(head==null){   // list empty
+                head=temp;
+                tail=temp;
+            }
+            else{
+                temp.next=head;
+                head=temp;
+            }
+            
+        }
+        void insertAtRandom(int idx,int val){
+            Node t=new Node(val);
+            Node temp=head;
+            if(idx==size()){
+                insertAtEnd(val);
+                return;
+            }
+            else if(idx==0){
+                insertAtStart(val);
+                return;
+            }
+            else if(idx<0 || idx>size()){
+                System.out.println("wrong index");
+                return;
+            }
+            for(int i=1;i<=idx-1;i++){
+                temp=temp.next;
+            }
+            t.next=temp.next;
+            temp.next=t;
+            
+            
+        }
+         
+        void deleteAt(int idx){
+            Node temp=head;
+            for(int i=1;i<=idx-1;i++){
+                temp=temp.next;
+            }
+            temp.next=temp.next.next;
+          
+        }
+       
+        void display(){
+            Node temp=head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+        }
+        int size(){
+            Node temp=head;
+            int count=0;
+            while(temp!=null){
+                count++;
+                temp=temp.next;
+            }
+            return count;
+        }
+    }
+    public static void main(String[] args) {
+        linkedlist l1= new linkedlist();
+        l1.insertAtStart(4); //4
+        l1.insertAtStart(9); // 9 -> 4
+        l1.insertAtStart(12); // 12->9->4
+        l1.insertAtStart(34);  //34->12->9->4
+        l1.insertAtRandom(2, 67);   // 34->12->67->9->4 ,    index start from 0
+        l1.insertAtRandom(0,100);   //100->34->12->67->9->4
+        l1.display();       
+        l1.deleteAt(4);
+        System.out.println();
+        l1.display();
+
+        
+    }
+    
+}
